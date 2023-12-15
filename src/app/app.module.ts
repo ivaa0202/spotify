26037ac651450b6185d4a08bd1b78c5d4a06a5ba
ivaa0202 <ivaa.0202@gmail.com>
2023-12-15 20:49:42 +0100

    late initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { FooterComponent } from './footer/footer.component';
+import { LanguagesComponent } from './nav-bar/languages/languages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PlaylistsComponent,
+    FooterComponent,
+    LanguagesComponent
   ],
   imports: [
     BrowserModule
